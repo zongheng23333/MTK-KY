e70beb2d56e419ca70f3b11657146030e41e7aa4
zongheng23333 <1954640276@qq.com>
2024-06-27 10:27:55 +0800

    Delete Patches directory

--- Patches/Patches.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $WRT_URL == *"immortalwrt"* && $WRT_TARGET == "Mediatek" ]]; then
-	mv -f ./Patches/mediatek/*.dts ./wrt/target/linux/mediatek/dts/
-
-	patch -R -p1 -d ./wrt < ./Patches/mediatek/mtk*.patch
-
-	echo "$WRT_TARGET patch has been installed!"
-fi
