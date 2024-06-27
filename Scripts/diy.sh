98b39cb0da2a57f09d610ea21c0a52183adc1eee
zongheng23333 <1954640276@qq.com>
2024-06-27 10:27:31 +0800

    Delete Scripts directory

--- Scripts/diy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-./scripts/feeds update -a 
-rm -rf feeds/luci/applications/luci-app-openclash
-cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
-
-./scripts/feeds install -a 
-
