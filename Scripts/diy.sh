88ecde2f94d3dfe55357a58785ea5aa83ac733be
zongheng23333 <1954640276@qq.com>
2024-06-29 16:47:07 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -4,5 +4,7 @@ sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a
 
-
+rm -rf   package/feeds/kenzo/luci-app-homeproxy
+rm -rf   package/feeds/small/sing-box
+rm -rf   package/feeds/small/shadowsocks-rust
 ./scripts/feeds install -a  
