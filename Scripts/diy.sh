cae3e14d016d1ed83f04dbbee526c854cd987c94
zongheng23333 <1954640276@qq.com>
2024-06-30 13:03:35 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -2,7 +2,9 @@
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 ./scripts/feeds update -a
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
-rm -rf feeds/smpackage/{luci-app-homeproxy,homeproxy,sing-box,shadowsocks-rust,v2ray-core,v2ray-plugin,v2rayA,Xray-core,xray-plugin}
+rm -rf feeds/package/{luci-app-homeproxy,homeproxy,sing-box,shadowsocks-rust,v2ray-core,v2ray-plugin,v2rayA,Xray-core,xray-plugin}
 
+rm -rf feeds/luci/applications/luci-app-openclash
+cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
 
 ./scripts/feeds install -a  
