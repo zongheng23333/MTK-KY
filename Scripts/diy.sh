76c5ad23ce321d54a03af94ee6a631f95d423cbb
zongheng23333 <1954640276@qq.com>
2024-09-24 10:46:15 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -4,4 +4,6 @@
 rm -rf feeds/luci/applications/luci-app-openclash
 cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
 
+rm -rf feeds/packages/net/*frp*
+git clone   -b    v0.42.0-1   https://github.com/kuoruan/openwrt-frp.git package/frp
 ./scripts/feeds install -a  
